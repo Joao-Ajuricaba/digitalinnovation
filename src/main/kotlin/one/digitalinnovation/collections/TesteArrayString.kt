11f15ec8001637b4beabcf1ee26bea86564a4a72
Joao-Ajuricaba <joaowagnergama@gmail.com>
2021-08-12 21:51:21 -0400

    created string array test class

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteArrayString.kt
@@ -0,0 +1,30 @@
+package one.digitalinnovation.collections
+
+fun main(){
+    val nomes = Array(3){""}
+
+    nomes[0] ="Eriane"
+    nomes[1] ="Sofia"
+    nomes[2] ="Isis"
+
+    for(nome in nomes){
+        println(nome)
+    }
+
+    println("----------")
+
+    nomes.sort()
+    nomes.forEach {
+        println(it)
+    }
+
+    println("----------")
+
+
+    val nomes2 = arrayOf("Eriane","Sofia","Isis")
+
+    nomes2.sort()
+    nomes2.forEach {
+        println(it)
+    }
+}
