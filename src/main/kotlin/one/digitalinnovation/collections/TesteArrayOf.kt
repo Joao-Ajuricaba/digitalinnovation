53d6df99d4ae1244ec20735b182b3ab71571ac69
Joao-Ajuricaba <joaowagnergama@gmail.com>
2021-08-16 19:34:17 -0400

    created  arrayOf of teste class

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteArrayOf.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.collections
+
+fun main(){
+    val values = intArrayOf(2,3,4,1,10,7)
+
+        values.forEach { valor -> println(valor) }
+
+    println("-----------")
+
+    values.sort()
+    values.forEach { println(it) }
+}
