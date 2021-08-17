5b831f48871295044b4e07201e6413e6c2be4fe8
Joao-Ajuricaba <joaowagnergama@gmail.com>
2021-08-17 10:01:35 -0400

    created list with sortedBy

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,20 +1,29 @@
 package one.digitalinnovation.collections
 
 fun main(){
-    val joao =Funcionarios("Joao",5000.0)
+    val joao =Funcionarios("Joao",500.0)
     val maria= Funcionarios("Maria",7500.0)
-    val ariadne=Funcionarios("Ariadne",10000.0)
+    val ariadne=Funcionarios("Ariadne",100.0)
 
     val FuncionariosLista = listOf(joao,maria,ariadne)
 
     FuncionariosLista.forEach {
-        println(it)
+        println(it)}
 
         println("-----------")
 
+    println(FuncionariosLista.find { it.nome =="Joao" })
 
-    }
-    println(FuncionariosLista.find { it.nome =="Jo" })
+    println("-----------")
+
+
+
+    FuncionariosLista
+        .sortedBy { it.salario }
+        .forEach{ println(it)}
+
+
+
 
 }
 
